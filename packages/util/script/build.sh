dd198ff9d8abe3975741d67b7c5d9db10feca675
awesome <awesome1888@gmail.com>
2019-08-31 21:42:29 +0200

    stuff

--- packages/util/script/build.sh
+++ packages/util/script/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_DIR=${DIR}/../build
 
-yarn;
-yarn run build;
+export NODE_ENV=build;
+rm -rf ${BUILD_DIR}/*;
+touch ${BUILD_DIR}/.gitkeep;
+babel ${DIR}/../src --ignore ${DIR}/../src/**/*.test.js --out-dir ${BUILD_DIR};
+cp ${DIR}/../package.json ${BUILD_DIR}/package.json;
