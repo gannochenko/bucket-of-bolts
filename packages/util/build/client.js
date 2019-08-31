24a6ca8fc20a7145c9d67b53bb2bbb97174664c6
Sergei <gannochenko.sv@gmail.com>
2019-08-31 18:10:37 +0200

    tests

--- packages/util/build/client.js
+++
@@ -1,7 +0,0 @@
-const settings = require('./lib/settings/client.js');
-
-const util = require('packages/util/src/etc.js');
-
-module.exports = { ...settings,
-  ...util
-};
--- packages/util/jest/config.js
+++ packages/util/jest/config.js
@@ -2,4 +2,5 @@ module.exports = {
   verbose: true,
   rootDir: '../src',
   setupFiles: ['<rootDir>/../jest/setup.js'],
+  testRegex: '\\.test\\.js$',
 };
