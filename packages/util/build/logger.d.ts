d99788fb21d39c250b046b90dd2c5add14ed7567
awesome <awesome1888@gmail.com>
2019-09-07 15:31:18 +0200

    another build

--- packages/util/build/logger.d.ts
+++ packages/util/build/logger.d.ts
@@ -1,6 +1,6 @@
 declare type Nullable<T> = T | null;
-export declare const debug: (message: string) => void;
-export declare const info: (message: string) => void;
-export declare const warn: (message: string) => void;
-export declare const error: (message: string, messageError?: Nullable<Error>) => void;
+export declare const logDebug: (message: string) => void;
+export declare const logInfo: (message: string) => void;
+export declare const logWarning: (message: string) => void;
+export declare const logError: (message: string, messageError?: Nullable<Error>) => void;
 export {};
--- packages/util/build/logger.js
+++ packages/util/build/logger.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.debug = function (message) {
+exports.logDebug = function (message) {
     console.log(JSON.stringify({ level: 'DEBUG', message: message }));
 };
-exports.info = function (message) {
+exports.logInfo = function (message) {
     console.log(JSON.stringify({ level: 'INFO', message: message }));
 };
-exports.warn = function (message) {
+exports.logWarning = function (message) {
     console.log(JSON.stringify({ level: 'WARNING', message: message }));
 };
-exports.error = function (message, messageError) {
+exports.logError = function (message, messageError) {
     if (messageError === void 0) { messageError = null; }
     var details = '';
     if (messageError instanceof Error && messageError.stack) {
