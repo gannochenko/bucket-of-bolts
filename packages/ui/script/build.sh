8697b38a29c4c22ab7bf0be9c44f21d1adec71fd
awesome <awesome1888@gmail.com>
2019-09-15 14:02:50 +0200

    update

--- packages/ui/script/build.sh
+++ packages/ui/script/build.sh
@@ -7,5 +7,4 @@ export NODE_ENV=build;
 rm -rf ${BUILD_DIR}/*;
 touch ${BUILD_DIR}/.gitkeep;
 babel ${DIR}/../src --ignore ${DIR}/../src/**/*.test.js --out-dir ${BUILD_DIR};
-cp ${DIR}/../package.json ${BUILD_DIR}/package.json;
 
--- packages/ui/script/prepublish.sh
+++ packages/ui/script/prepublish.sh
@@ -3,6 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_DIR=${DIR}/../build
 
-rm -rf ${BUILD_DIR}/*;
-touch ${BUILD_DIR}/.gitkeep;
-npx tsc --p ${DIR}/../
+cp ${DIR}/../package.json ${BUILD_DIR}/package.json;
