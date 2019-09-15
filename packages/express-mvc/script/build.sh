a40dee28db9aa580eab90ef748b5f3548a950cc0
Sergei <gannochenko.sv@gmail.com>
2019-09-15 11:22:31 +0200

    trying to solve

--- packages/express-mvc/script/build.sh
+++ packages/express-mvc/script/build.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRC_DIR=${DIR}/../src
 BUILD_DIR=${DIR}/../build
 
-export NODE_ENV=build;
 rm -rf ${BUILD_DIR}/*;
 touch ${BUILD_DIR}/.gitkeep;
-babel ${SRC_DIR} --ignore ${SRC_DIR}/**/*.test.js --out-dir ${BUILD_DIR} --extensions ".ts,.js";
-cd ${BUILD_DIR};
-ln -s ../package.json ./;
-rm -f ./*.d.js
-cp -f ${SRC_DIR}/index.d.ts ./;
+npx tsc --p ${DIR}/../
+
