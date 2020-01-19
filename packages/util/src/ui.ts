44fcacff469d4955c06a629f491766c1048866dc
Sergei <gannochenko.sv@gmail.com>
2020-01-19 20:54:44 +0000

    wip

--- packages/util/src/ui.ts
+++ packages/util/src/ui.ts
@@ -13,3 +13,19 @@ export const hasParentNode = (node: HTMLElement, parent: HTMLElement) => {
 
     return false;
 };
+
+export const getSelectedText = () => {
+    let text = '';
+    if (window.getSelection) {
+        const selection = window.getSelection();
+        if (selection) {
+            text = selection.toString();
+        }
+        // @ts-ignore
+    } else if (document.selection && document.selection.type !== 'Control') {
+        // @ts-ignore
+        // eslint-disable-next-line prefer-destructuring
+        text = document.selection.createRange().text;
+    }
+    return text;
+};
